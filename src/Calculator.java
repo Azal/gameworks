19e296181b14221eae5afdf998c9a855dd90de92
John Peebles <jpeeblesg@gmail.com>
2013-08-13 11:10:30 -0400

    calculadora

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,21 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 
 public class Calculator {
-
+	public Object parse(String Message){
+		ScriptEngineManager mgr = new ScriptEngineManager();
+	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+	    try {
+			return engine.eval(Message);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return -1;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Ingresa qué quieres calcular: ");
 		Calculator calc=new Calculator();
-		try{
-			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-			String s = bufferRead.readLine();
-			
-			System.out.println(calc.parse(s));
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
+		while(true){
+			try{
+				BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+				String s = bufferRead.readLine();
+
+				System.out.println(calc.parse(s));
+				//System.out.println(s);
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 }
