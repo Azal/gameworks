79bd6e3c9eb11692588c31a31141da25ff1eb3dc
John Peebles <jpeeblesg@gmail.com>
2013-08-13 11:14:44 -0400

    -empty-

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Ingresa qué quieres calcular: ");
 		Calculator calc=new Calculator();
-		while(true){
+		String mensaje="";
+		while(!mensaje.equals("salir")){
 			try{
 				BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-				String s = bufferRead.readLine();
-
-				System.out.println(calc.parseJS(s));
+				 mensaje = bufferRead.readLine();
+				 if(!mensaje.equals("salir"))
+				System.out.println(calc.parseJS(mensaje));
 				//System.out.println(s);
 			}
 			catch(IOException e)
