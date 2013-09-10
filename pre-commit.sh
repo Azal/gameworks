3f226b7788a11dd18b512b473a32b86b928bb9a5
Max Friedrich Findel <maxfindel@gmail.com>
2013-09-10 12:17:02 -0300

    yes or no?

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,3 +1,15 @@
-echo "Pre-Commit Message"; 
+echo "Hello";
+confirmation="0"
+# while true; do
+#     read -p "Are you sure you want to push to master? (y/n) : " yn
+#     case $yn in
+#         [Yy]* ) confirmation=1; break;;
+#         [Nn]* ) confirmation=0; break;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
+read -p "Are you sure you want to push to master? (y/n) : " confirmation
+echo "Pre-Commit Message $confirmation";
 exit
 echo "Pre-Commit Message2"; 
+
