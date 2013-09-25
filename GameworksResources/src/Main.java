a126fa19f042bc810a110301f85b3341a3e15974
Ignacio Carmach <icarmach@gmail.com>
2013-09-25 17:54:07 -0300

    base code for every resource

--- GameworksResources/src/Main.java
+++ GameworksResources/src/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SoundFactory sf = new SoundFactory();
+		
+		ISound sound = sf.createSound("path");
+		sound.Play();
 	}
 
 }
