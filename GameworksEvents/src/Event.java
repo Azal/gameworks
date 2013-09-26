a7c5e8006e032414dead3c4fbdc07c819dd9a8cf
Juan Jaime Diaz <squirrelmcnutsy@gmail.com>
2013-09-26 17:34:16 -0400

    Started working on the EventManager and Event classes

---
+++ GameworksEvents/src/Event.java
@@ -0,0 +1,11 @@
+
+public class Event {
+	// new Event(String identifier, List<Restriction> Restrictions, List<Result> Results);
+	
+	public boolean getStatus()
+	{
+		//Iterate through the list of restrictions
+		//If all restrictions have been completed, return true.
+		return true;
+	}
+}
