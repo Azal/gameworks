fb2e800876cca300105918b3f10e93c5d2de2fbd
slakat <slakat@gmail.com>
2013-09-26 10:35:31 -0300

    fixed

--- PersistenceModule/src/persistenceModule/SaveProjectController.java
+++ PersistenceModule/src/persistenceModule/SaveProjectController.java
@@ -1,5 +1,87 @@
 package persistenceModule;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+ 
+
 
 public class SaveProjectController {
+	
+public SaveProjectController(){
+	save(); System.out.print("started");
+	
+}
+	
+	
+public void save(){
+	JSONObject obj = new JSONObject();
+	obj.put("name", "mkyong.com");
+	obj.put("age", new Integer(100));
+ 
+	JSONArray list = new JSONArray();
+	list.add("msg 1");
+	list.add("msg 2");
+	list.add("msg 3");
+ 
+	obj.put("messages", list);
+ 
+	try {
+ 
+		FileWriter file = new FileWriter("c:\\test.json");
+		file.write(obj.toJSONString());
+		file.flush();
+		file.close();
+ 
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+ 
+	System.out.print(obj);
+}
+
+public void read(String args){
+	JSONParser parser = new JSONParser();
+	 
+	try {
+ 
+		Object obj = parser.parse(new FileReader("c:\\test.json"));
+ 
+		JSONObject jsonObject = (JSONObject) obj;
+ 
+		String name = (String) jsonObject.get("name");
+		System.out.println(name);
+ 
+		long age = (Long) jsonObject.get("age");
+		System.out.println(age);
+ 
+		// loop array
+		JSONArray msg = (JSONArray) jsonObject.get("messages");
+		Iterator<String> iterator = msg.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+ 
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ParseException e) {
+		e.printStackTrace();
+	}
+ 
+}
+
+
+
 
 }
