2ae8df8156acfe2c56e8e82434ee9482ebac0b53
unknown <neeco.gmz@gmail.com>
2013-09-27 01:41:41 -0400

    added update and events methods

--- src/Main/GameThread.java
+++ src/Main/GameThread.java
@@ -1,9 +1,11 @@
+package Main;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class GameThread extends Thread {
 	
 	private Game game;
+	private long lastLoopTime;
 	
 	public GameThread(Game game)
 	{
@@ -11,24 +13,47 @@ public class GameThread extends Thread {
 	}
 	
 	public void run() {
-		long lastLoopTime = System.currentTimeMillis();
+		lastLoopTime = System.currentTimeMillis();
+		
 		while(true)
 		{
 			try { Thread.sleep(10); } catch (Exception e) {}
-			long delta = System.currentTimeMillis() - lastLoopTime;
-			lastLoopTime = System.currentTimeMillis();
 			
-			Graphics2D g = (Graphics2D) game.GetStrategy().getDrawGraphics();
-			g.setColor(Color.black);
-			g.fillRect(0,0,800,600);
-			
-			
-			g.setColor(Color.white);
-			g.drawString("FPS: " + 1000/delta, 50, 50);
-			
-			g.dispose();
-			game.GetStrategy().show();
+			update();
+			events();
 		}
 	}
-
+	
+	
+	/**
+	 * Update in map elements (player and NPC positions, objects, bullets, animations, etc)
+	 * @author nggomez, timartin
+	 */
+	private void update(){
+		// Example implementation
+		long delta = System.currentTimeMillis() - lastLoopTime;
+		lastLoopTime = System.currentTimeMillis();
+		
+		Graphics2D g = (Graphics2D) game.GetStrategy().getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0,0,800,600);
+		
+		g.setColor(Color.white);
+		
+		g.drawString("FPS: "+1000/delta, 50, 50);
+		g.drawString("Time: "+game.getGameTime()/1000, 50, 80);
+		
+		
+		g.dispose();
+		game.GetStrategy().show();
+	}
+	
+	/**
+	 * Fire other type of events
+	 * @author nggomez
+	 */
+	private void events(){
+		
+	}
+	
 }
