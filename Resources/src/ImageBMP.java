c2e6127e8bc4e9ef277a46391317b107a9bbf1e0
Mauricio Vera <mverapino@gmail.com>
2013-09-27 10:54:28 -0300

    Agrego imagenes a assets y clases para formatos BMP GIF y PNG

---
+++ Resources/src/ImageBMP.java
@@ -0,0 +1,16 @@
+package Resources.src;
+
+
+public class ImageBMP implements IImage{
+
+
+    private String filename;
+
+	public ImageBMP(String path) {
+		filename = path;
+	}
+	
+	public void Rotate() {
+		//rotate
+	}
+}
