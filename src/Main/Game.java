0ac4ab9da5356cc533dd351ec3afc3b8f1f769f7
unknown <neeco.gmz@gmail.com>
2013-09-27 01:40:42 -0400

    input from keyboard added

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,11 +1,25 @@
-import java.awt.Color;
+package Main;
+
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
+
+import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.KeyStroke;
+
+/**
+-Class description goes here.-
+	-Reference to external code-
+*
+*	@version  0.1
+@created_by @group6/timartin
+@update_log
+	27/09/2013 - @group6/timartin, nggomez        
+*/
+
+/**  
+main documentation comment */
 
 public class Game {
 	
@@ -15,10 +29,14 @@ public class Game {
 	private JPanel panel;
 	private BufferStrategy strategy;
 	
+	private long startedAt;
+	
 	private Game(String name, int width, int height)
 	{
 		container = new JFrame(name);
-
+		container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		container.setResizable(false);
+		
 		panel = (JPanel) container.getContentPane();
 		panel.setPreferredSize(new Dimension(width,height));
 		panel.setLayout(null);
@@ -32,8 +50,20 @@ public class Game {
 		container.createBufferStrategy(2);
 		
 		strategy = container.getBufferStrategy();
+		
+		startedAt = System.currentTimeMillis();
+		
 	}
 	
+	/**
+	 * Public constructor for Game
+	 * @param name this name will appears in the window
+	 * @param width of the Game Window
+	 * @param height of the Game Window
+	 * @return Game Object
+	 * @throws Exception if the game already exists
+	 * @author timartin
+	 */
 	public static Game Create(String name, int width, int height) throws Exception
 	{
 		if(game == null)
@@ -48,6 +78,12 @@ public class Game {
 		return game;
 	}
 
+	
+	/**
+	 * Returns the unique instance of Game class (Singleton)
+	 * @return Game
+	 * @throws Exception
+	 */
 	public static Game GetGame() throws Exception
 	{
 		if(game == null)
@@ -57,6 +93,11 @@ public class Game {
 		return game;
 	}
 	
+	/**
+	 * Shows the Game windows
+	 * @author timartin
+	 * @throws Exception
+	 */
 	public void Init() throws Exception
 	{
 		container.setVisible(true);
@@ -69,5 +110,21 @@ public class Game {
 	public BufferStrategy GetStrategy()
 	{
 		return strategy;
+	}
+	
+	public long getGameTime(){
+		return System.currentTimeMillis()-startedAt;
+	}
+	
+	
+	/**
+	 * Register an action callback to a Keyboard key press.
+	 * @param key is the keyboard key, for example: "SPACE", "ENTER", etc
+	 * @param action Instance of a class that is derived from AbstractAction
+	 */
+	public void onKeyPress(String key, Action action){
+		KeyStroke keyStroke = KeyStroke.getKeyStroke(key);
+		panel.getInputMap().put(keyStroke, key);
+		panel.getActionMap().put(key, action);
 	}
 }
