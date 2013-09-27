708d5aad03fd92ca197969ee8a9804bb193ed86a
Nikolas Bravo <nikolasbra@gmail.com>
2013-09-26 21:57:57 -0500

    added scenery package

---
+++ src/scenery/Map.java
@@ -0,0 +1,19 @@
+package scenery;
+
+/**
+-Here reside all objects and characters that compose the Map-
+ 	-Reference to external code-
+*
+*	@version  0.1
+@created_by @group5/,,,ngbravo
+@update_log
+	26/09/2013 - @group5/ngbravo
+	dd/mm/yyyy - @groupX/author[,author2,...]          
+*/
+
+/**  
+main documentation comment */
+
+public class Map {
+
+}
