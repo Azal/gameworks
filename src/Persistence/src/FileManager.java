3779bef35af5fddded2cd0614f50cc04b9388d29
Francisco Salinas <fcosalinas1987@gmail.com>
2013-10-14 17:13:05 -0300

    Implementada la clase FileManager

--- src/Persistence/src/FileManager.java
+++ src/Persistence/src/FileManager.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.plaf.FileChooserUI;
+
 
 /**
 -find and updates files path or create them if don't exists
@@ -11,6 +18,8 @@
 
 public class FileManager implements IFile  {
 
+	List<File> results = null;
+	
 	@Override
 	public boolean isFile(String name) {
 		// TODO Auto-generated method stub
@@ -18,15 +27,72 @@ public class FileManager implements IFile  {
 	}
 
 	@Override
-	public String findFile(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public String findFile(File root, String name) {		
+
+		 results = new ArrayList<File>();//Cleans the results list at the beginning
+		 
+		if(!root.isDirectory()){
+		    throw new IllegalArgumentException("File root is not a directory");
+		  }
+		  File[] files = root.listFiles();
+		  for(int i=0; i<files.length; i++){
+		    File file = files[i];
+		    //if is directory, it starts to search in this directory
+		    if(file.isDirectory()){
+		      findFile(file, name);
+		    }
+		    //It compares to the file's name
+		    if(file.getName().matches(name)){
+		      addResult(file);
+		    }
+		  }
+		 
+		if(!results.isEmpty())
+			return results.get(0).getAbsolutePath();
+		else
+			return null;
 	}
 
 	@Override
-	public boolean newFile(Object o) {
-		// TODO Auto-generated method stub
+	public boolean newFile(File path, Object o) {
+		
+		if(!path.isDirectory()){
+		    throw new IllegalArgumentException("File root is not a directory");
+		 }
+		
+		File f = null;
+		boolean bool;
+	      
+	      try{
+	         // create new file
+	         f = new File((File)o, path.getAbsolutePath());
+	         
+	         // tries to create new file in the system
+	         bool = f.createNewFile();
+	         
+	         // prints
+	         System.out.println("File created: "+bool);
+	            
+	      }catch(Exception e){
+	         e.printStackTrace();
+	      }
+			
+		
+		
+		
 		return false;
+	}
+	
+	//Print the path to the found files
+	private void showResults(){
+	  for(File archivo : results){
+	    System.out.println(archivo.getAbsolutePath());
+	  }
+	}
+	
+	//Add a file to the search findings
+	private void addResult(File file){
+	  results.add(file);
 	}
 	
 	
--- src/Persistence/src/IFile.java
+++ src/Persistence/src/IFile.java
@@ -1,12 +1,14 @@
+import java.io.File;
+
 
 public interface IFile {
 	
 	
 	public boolean isFile(String name);
 	
-	public String findFile(String name);
+	public String findFile(File root, String name);
 	
-	public boolean newFile(Object o);
+	public boolean newFile(File path, Object o);
 
 	/**class bottom**/	
 }
