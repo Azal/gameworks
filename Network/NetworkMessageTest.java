cf00dc7491942ebc156c759dcb259986cf5c0554
Max Friedrich Findel <maxfindel@gmail.com>
2013-10-22 10:19:10 -0300

    moving outisde of src

---
+++ Network/NetworkMessageTest.java
@@ -0,0 +1,16 @@
+package src.Network;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NetworkMessageTest {
+
+	@Test
+	public void test() {
+		NetworkMessage nm = new NetworkMessage(0,"conversation", "content");
+		assertNotNull(nm);
+		assertEquals(nm.parse(nm.getString()).getString(),nm.getString() );
+	}
+
+}
