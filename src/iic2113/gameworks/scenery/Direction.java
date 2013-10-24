9bc293febf37bed1c57144bf3597a4a12bc0fe9a
Nikolas Bravo <nikolasbra@gmail.com>
2013-10-24 10:55:08 -0500

    Enum Direction created. Class Sprite ready.

---
+++ src/iic2113/gameworks/scenery/Direction.java
@@ -0,0 +1,5 @@
+package iic2113.gameworks.scenery;
+
+enum Direction {
+	UP, DOWN, LEFT, RIGHT
+}
--- src/iic2113/gameworks/scenery/Sprite.java
+++ src/iic2113/gameworks/scenery/Sprite.java
@@ -1,15 +1,83 @@
 package iic2113.gameworks.scenery;
 
+import javax.swing.JLabel;
+
 /**
-Here reside blabla
+All characters and object inherit from this class
  	-Reference to external code-
-*
-*	@version  0.1
+ *
+ *	@version  0.1
 @created_by @group5/gevalenz,javicabello,ngbravo,vibaseta
 @update_log
 	26/09/2013 - @group5/ngbravo,gevalenz,javicabello,vibaseta      
-*/
+ */
 
-class Sprite {
+abstract class Sprite {
+
+	protected JLabel spritePath;
+	protected int posX;
+	protected int posY;
+
+	/**
+	 * 
+	 * @return
+	 */
+	JLabel getSpritePath(){
+		return spritePath;
+	}
+
+	/**
+	 * 
+	 * @param spritePath
+	 */
+	void setSpritePath(JLabel spritePath){
+		this.spritePath=spritePath;
+	}
+
+	/**
+	 * Method to move the Sprite in a given direction and distance
+	 * @param direction Direction of the movement. Can be UP, DOWN, LEFT, RIGHT
+	 * @param distance How many grid spaces will it move in the given direction
+	 */
+	void move(Direction direction, int distance){
+		switch (direction) {
+		case UP:
+			posY+=distance;
+			break;
+
+		case DOWN:
+			posY-=distance;
+			break;	
+
+		case LEFT:
+			posX-=distance;
+			break;	
+
+		case RIGHT:
+			posX+=distance;
+			break;
+		}
+	}
+
+	/**
+	 * Get the position of the Sprite
+	 * @return Returns a int array of length=2. The position X will be in [0] and position Y will be in [1].
+	 */
+	int[] getPosition(){
+		int[] position=new int[2];
+		position[0]=posX;
+		position[1]=posY;
+		return position;
+	}
+
+	/**
+	 * Setter for positions
+	 * @param positionX new position X
+	 * @param positionY new position Y
+	 */
+	void setPosition(int positionX, int positionY){
+		posX=positionX;
+		posY=positionY;
+	}
 
 }
