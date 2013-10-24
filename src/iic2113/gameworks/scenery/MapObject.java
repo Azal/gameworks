a0a0771118dc146ebfbb61c2b1fe77486da0a344
javicabello <jacabell@uc.cl>
2013-10-24 16:22:36 -0300

    Arreglos en comentarios de clases nuevas

--- src/iic2113/gameworks/scenery/MapObject.java
+++ src/iic2113/gameworks/scenery/MapObject.java
@@ -9,7 +9,7 @@ All characters and object inherit from this class
  *	@version  0.1
 @created_by @group5/gevalenz,javicabello,ngbravo,vibaseta
 @update_log
-	26/09/2013 - @group5/ngbravo,gevalenz,javicabello,vibaseta      
+	22/10/2013 - @group5/ngbravo,gevalenz,javicabello,vibaseta      
  */
 
 abstract class MapObject {
