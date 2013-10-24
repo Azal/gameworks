a8c2c5d60c1fd97071c3359687dd0969052c4abe
Giovanni De Lucca <giovanni@delucca.cl>
2013-10-24 10:25:10 -0400

    Cambiado nuevamente los package a iic2113.gameworks

--- src/CharacterBehaviour/search/tieBreakers/TieBreaker.java
+++
@@ -1,8 +0,0 @@
-package src.CharacterBehaviour.search.tieBreakers;
-
-import src.CharacterBehaviour.search.AState;
-
-public interface TieBreaker
-{
-	AState breakTie(AState state1, AState state2);
-}
