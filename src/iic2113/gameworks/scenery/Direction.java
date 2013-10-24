22174d1997d8b15ff9dbc5e6405d8c01510ff684
Nikolas Bravo <nikolasbra@gmail.com>
2013-10-24 11:00:53 -0500

    Documentation for Direction ready

--- src/iic2113/gameworks/scenery/Direction.java
+++ src/iic2113/gameworks/scenery/Direction.java
@@ -1,5 +1,16 @@
 package iic2113.gameworks.scenery;
 
+/**
+Enum for movement directions
+ 	-Reference to external code-
+ *
+ *	@version  0.1
+@created_by @group5/gevalenz,javicabello,ngbravo,vibaseta
+@update_log
+	24/10/2013 - @group5/ngbravo,gevalenz,javicabello,vibaseta      
+ */
+
+
 enum Direction {
 	UP, DOWN, LEFT, RIGHT
 }
